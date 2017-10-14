52e8749d9e931f72252c649519d45d0e968b9601
R. Murphy <e60369@gmail.com>
2017-10-14 19:45:17 -0400

    Final version

--- Weather_Balloon/MURPHY_P2.cpp
+++ Weather_Balloon/MURPHY_P2.cpp
@@ -8,7 +8,9 @@
 // Purpose : This program outputs weather balloon altitude and velocity as
 // a function of time based on some user inputs.
 
-// Note:
+// Note: The system ("CLS") command doesn't work in Xcode. As a workaround I've
+// created another function called clr_scrn() which simulates the system ("CLS")
+// command by performing 10 "\n" commands.
 //----------------------------------------------------------------------------
 
 #include <iostream>
@@ -18,13 +20,13 @@
 
 using namespace std;
 
-void clr_scrn()
+void clr_scrn() // See comment above regarding system ("CLS") command in Xcode.
 {
     for (int i = 1; i < 11; i++)
         cout << "\n" << endl;
 }
 
-float myAltitude (float total_time, float WB)
+float myAltitude (float total_time, float WB) // This is our altitude function.
     {
     float W1A1=-.12, W1B1=12, W1C1=-380, W1D1=4100, W1E1=220;
     float W2A1=-.11, W2B1=10, W2C1=-235, W2D1=3850, W2E1=178;
@@ -42,7 +44,7 @@ float myAltitude (float total_time, float WB)
         return 0;
     }
 
-float myVelocity (float total_time, float WB)
+float myVelocity (float total_time, float WB) // This is our velocity function.
     {
     float W1A2=0, W1B2=-.48, W1C2=36, W1D2=-760, W1E2=4100;
     float W2A2=0, W2B2=34, W2C2=34, W2D2=-564, W2E2=3450;
@@ -60,7 +62,7 @@ float myVelocity (float total_time, float WB)
     return 0;
     }
 
-void mymenu ()
+void mymenu () // This is the menu which greets the user after they enter the simulation parameters.
 {
     cout << "\t\tSELECT THE BALLOON FOR SIMULATION" << endl;
     cout << "\t1. Weather Balloon 1" << endl;
@@ -69,7 +71,7 @@ void mymenu ()
     cout << "\t4. Exit" << endl;
 }
 
-int main()
+int main() // Our main program consists of two do-loops, one for the parameters and one for the main menu.
 {
     float t_init,t_final,t_incr,total_time;
     float WB=0;
@@ -88,7 +90,7 @@ int main()
     cin >> t_incr;
     cout << "\tEnter the final value of the observation (in hrs): ";
     cin >> t_final;
-    if (t_final - t_init <5 || t_final - t_init >24)
+    if (t_final - t_init <5 || t_final - t_init >24) // This is the circuit breaker if the user enters in too short or too long a simulation.
         {
         cout << endl;
         cout << "NOT PROPER DURATION. Observation period must be between 5 and 24 hours." << endl;
@@ -96,7 +98,7 @@ int main()
         }
     } while (t_final - t_init <5 || t_final - t_init >24);
     
-    do
+    do // This do-loop controls the selection, reading, and writing of the data.
     {
         cout << endl;
         mymenu();
@@ -105,41 +107,50 @@ int main()
     if (WB == 1)
         {
         ofstream data1("Murphy_WB1.txt");
-        data1 << setprecision (2) << fixed;
-        cout << "Weather balloon 1 data saved to Murphy_WB1.txt" << endl;
+        data1 << setprecision (2) << fixed; // We are rounding off at 2 decimal places like the example in the instructions.
         cout <<endl;
                 for (total_time = t_init; total_time <= t_final; total_time += t_incr)
                     {
                     data1 << total_time << "\t" << setw(10) << myAltitude(total_time, WB)<< "\t" << setw(5)<< myVelocity(total_time, WB) << endl;
                     }
+            clr_scrn();
+            cout << "Weather balloon 1 data saved to Murphy_WB1.txt" << endl;
+            cout << "Please make another selection" << endl;
             }
     
             else if (WB == 2)
                     {
                     ofstream data2("Murphy_WB2.txt");
-                    data2 << setprecision (2) << fixed;
-                    cout << "Weather balloon 2 data saved to Murphy_WB2.txt" << endl;
+                    data2 << setprecision (2) << fixed; // We are rounding off at 2 decimal places like the example in the instructions.
+
                     cout <<endl;
                             for (total_time = t_init; total_time <= t_final; total_time += t_incr)
                                 {
                                     data2 << total_time << "\t" << setw(10) << myAltitude(total_time, WB)<< "\t" << setw(5)<< myVelocity(total_time, WB) << endl;
                                 }
+                        clr_scrn();
+                        cout << "Weather balloon 2 data saved to Murphy_WB2.txt" << endl;
+                        cout << "Please make another selection" << endl;
                     }
     
                     else if (WB == 3)
                             {
                             ofstream data3("Murphy_WB3.txt");
-                            data3 << setprecision (2) << fixed;
-                            cout << "Weather balloon 3 data saved to Murphy_WB3.txt" << endl;
+                            data3 << setprecision (2) << fixed; // We are rounding off at 2 decimal places like the example in the instructions.
+                           
                             cout <<endl;
                                     for (total_time = t_init; total_time <= t_final; total_time += t_incr)
                                         {
                                         data3 << total_time << "\t" << setw(10) << myAltitude(total_time, WB)<< "\t" << setw(5)<<   myVelocity(total_time, WB) << endl;
                                         }
+                                clr_scrn();
+                                cout << "Weather balloon 3 data saved to Murphy_WB3.txt" << endl;
+                                cout << "Please make another selection" << endl;
                             }
     
                             else if (WB == 4)
                                     {
+                                    clr_scrn();
                                     cout << "--------------------------" <<endl;
                                     cout << "\tEnd of Project #2" <<endl;
                                     cout << "--------------------------" <<endl;
@@ -149,8 +160,9 @@ int main()
                                     else if (WB != 4)
                                         {
                                         cout << "Error, please select from only the menu items below." << endl;
+                                        cout << endl;
                                         }
-            } while (WB !=4);
+            } while (WB !=4); // Loop will run until user selects 4 to exit.
     
 return 0;
     
